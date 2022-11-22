79dad2b5fe91bbaae680a72925211611815c99ec
Kendall-Marshall <kmanmarshall@yahoo.com>
2022-11-22 20:53:57 +1300

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app>
+    <v-container fill-height>
+      <v-row align="center"
+        justify="center">
+        <v-col v-for="site in sites" :key="site.id"
+          cols="12"
+          sm="3">
+          <Card :title="site"/>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+  import Card from './components/Card';
+  import sitesJson from './data/sites.json';
+
+  export default {
+    name: 'App',
+
+    components: {
+      Card
+    },
+
+    methods: {
+      getBgImg() {
+        return require('./Images/bg.jpg')
+      }
+    },
+
+    data() {
+      return {
+        sites: sitesJson
+      }
+      
+    }
+
+  };
+</script>
+
+<style>
+#app {
+  background: url('./Images/bg.jpg')
+    no-repeat center center fixed !important;
+  background-size: cover;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card
+    class="ma-md-4 mx-lg-auto"
+    max-width="400"
+    @click.middle="newTabPressEvent(title.siteURL)"
+    @click.native="pressEvent(title.siteURL)">
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      :src="getImgUrl(title.Image)">
+      <v-card-title>{{title.Name}}</v-card-title>
+    </v-img>
+  </v-card>
+</template>
+
+<script>
+ export default {
+    props:['title'],
+    methods: {
+        pressEvent : function (url) {
+          window.location.href = url
+        },
+        newTabPressEvent : function (url) {
+          window.open(url, '_blank');
+        },
+        getImgUrl(pic) {
+          return require('../Images/'+pic)
+        }
+    },
+ }
+</script>
